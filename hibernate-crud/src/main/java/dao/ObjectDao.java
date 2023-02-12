55b16371545896929654520a78db229261579565
Chaitanya Kulkarni <chaitanyakulkarni789@gmail.com>
2023-02-12 14:33:32 +0530

    merge()

--- hibernate-crud/src/main/java/dao/ObjectDao.java
+++ hibernate-crud/src/main/java/dao/ObjectDao.java
@@ -7,54 +7,62 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ObjectDao {
-    public static SessionFactory sessionFactory = null;
+  public static SessionFactory sessionFactory = null;
+  public static Session session = null;
+  List<User> users;
+  public static Session getSession() {
+    sessionFactory = HibernateConfig.getSessionFactory();
+    session = sessionFactory.openSession();
+    return session;
+  }
 
-    public static Session getSession() {
-        sessionFactory = HibernateConfig.getSessionFactory();
-        Session session = sessionFactory.openSession();
-        return session;
-    }
+  public void saveObject(Object o) {
+    session = getSession();
+    Transaction transaction = session.beginTransaction();
+    session.persist(o);
+    transaction.commit();
+    session.close();
+  }
 
-    public void saveObject(Object o) {
-        Session session = getSession();
-        Transaction transaction = session.beginTransaction();
-        session.persist(o);
-        transaction.commit();
-        session.close();
-    }
+  public void updateDetails(Object o) {
+    session = getSession();
+    Transaction transaction = session.beginTransaction();
+    session.saveOrUpdate(o);
+    transaction.commit();
+    session.close();
+  }
 
-    public void updateDetails(Object o) {
-        Session session = getSession();
-        Transaction transaction = session.beginTransaction();
-        session.saveOrUpdate(o);
-        transaction.commit();
-        session.close();
-    }
+  public void mergeUser(Object o) {
+    session = getSession();
+    Transaction transaction = session.beginTransaction();
+    session.merge(o);
+    transaction.commit();
+    session.close();
+  }
 
-    public User getSingleUser(int userid) {
-        Session session = getSession();
-        User user;
-        user = session.get(User.class, userid);
-        return user;
-    }
+  public User getSingleUser(int userid) {
+    session = getSession();
+    User user;
+    user = session.get(User.class, userid);
+    return user;
+  }
 
-    public List<User> getUsers() {
-        Session session = getSession();
-        Query<User> query = session.createQuery("from User", User.class);
-        List<User> users = query.getResultList();
-        return users;
-    }
+  public List<User> getUsers() {
+    session = getSession();
+    Query<User> query = session.createQuery("from User", User.class);
+    users = query.getResultList();
+    return users;
+  }
 
-    public void deleteUser(User user) {
-        Session session = getSession();
-        Transaction transaction = session.beginTransaction();
-        session.remove(user);
-        transaction.commit();
-        session.close();
-    }
+  public void deleteUser(User user) {
+    session = getSession();
+    Transaction transaction = session.beginTransaction();
+    session.remove(user);
+    transaction.commit();
+    session.close();
+  }
 }
 
--- hibernate-crud/src/main/java/mainapp/MainApp.java
+++ hibernate-crud/src/main/java/mainapp/MainApp.java
@@ -1,6 +1,5 @@
 package mainapp;
 
-import config.HibernateConfig;
 import dao.ObjectDao;
 import dummydata.DummyUserProvider;
 import model.User;
@@ -8,56 +7,68 @@ import model.User;
 import java.util.List;
 
 public class MainApp {
-    public static void main(String[] args) {
-        ObjectDao objectDao = new ObjectDao();
-        User user;
-        /**
-         * Insert user
-         * */
-        System.out.println("---------------------------------------------------------------------------------------------");
-        user = getNewUser();
-        objectDao.saveObject(user);
-        System.out.println("---------------------------------------------------------------------------------------------");
-        /**
-         * delete user using userid
-         * */
-        System.out.println("---------------------------------------------------------------------------------------------");
-        System.out.println("Deleting user.\n"+user);
-        objectDao.deleteUser(user);
-        System.out.println("---------------------------------------------------------------------------------------------");
-        /**
-         * Update user
-         * */
-        System.out.println("---------------------------------------------------------------------------------------------");
-        user = getNewUser();
-        user.setId(1);
-        objectDao.updateDetails(user);
-        System.out.println("---------------------------------------------------------------------------------------------");
-        /**
-         * fetch single user
-         * */
-        System.out.println("---------------------------------------------------------------------------------------------");
-        user = objectDao.getSingleUser(1);
-        System.out.println(user);
-        System.out.println("---------------------------------------------------------------------------------------------");
-        /**
-         * get all users
-         * */
-        System.out.println("---------------------------------------------------------------------------------------------");
-        List<User> users = objectDao.getUsers();
-        users.stream().forEach(System.out::println);
-        System.out.println("---------------------------------------------------------------------------------------------");
+  public static void main(String[] args) {
+    ObjectDao objectDao = new ObjectDao();
+    User user,user2;
+    /*
+     * Insert user
+     * */
 
-    }
+      System.out.println("---------------------------------------------------------------------------------------------");
+      user = getNewUser();
+      user2=user;
+      objectDao.saveObject(user);
 
-    public static User getNewUser() {
-        DummyUserProvider provider = new DummyUserProvider();
-        User user = new User();
-        user.setName(provider.getFakeEmployeeNameFull());
-        user.setEmailId(provider.getEmailId());
-        user.setPhoneNumber(provider.getPhoneNumber());
-        user.setAddress(provider.getAddress());
-        return user;
-    }
+    System.out.println("---------------------------------------------------------------------------------------------");
+    /*
+     * delete user using userid
+     * */
+    System.out.println("---------------------------------------------------------------------------------------------");
+    System.out.println("Deleting user.\n" + user2);
+    objectDao.deleteUser(user2);
+    System.out.println("---------------------------------------------------------------------------------------------");
+    /*
+     * Update user
+     * */
+//    System.out.println("---------------------------------------------------------------------------------------------");
+//    user = getNewUser();
+//    user.setId(1);
+//    objectDao.updateDetails(user);
+//    System.out.println("---------------------------------------------------------------------------------------------");
+    /*
+     * merge() in hibernate
+     * */
+    System.out.println("---------------------------------------------------------------------------------------------");
+    user = getNewUser();
+    user.setId(2);
+    objectDao.mergeUser(user);
+    System.out.println("---------------------------------------------------------------------------------------------");
+
+    /*
+     * fetch single user
+     * */
+    System.out.println("---------------------------------------------------------------------------------------------");
+    user = objectDao.getSingleUser(1);
+    System.out.println(user);
+    System.out.println("---------------------------------------------------------------------------------------------");
+    /*
+     * get all users
+     * */
+    System.out.println("---------------------------------------------------------------------------------------------");
+    List<User> users = objectDao.getUsers();
+    users.forEach(System.out::println);
+    System.out.println("---------------------------------------------------------------------------------------------");
+
+  }
+
+  public static User getNewUser() {
+    DummyUserProvider provider = new DummyUserProvider();
+    User user = new User();
+    user.setName(provider.getFakeEmployeeNameFull());
+    user.setEmailId(provider.getEmailId());
+    user.setPhoneNumber(provider.getPhoneNumber());
+    user.setAddress(provider.getAddress());
+    return user;
+  }
 
 }
